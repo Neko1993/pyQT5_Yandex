28ff3890975a5ce8e609dea46200be3ddd1b1488
Neko1993 <koshka1993@xakep.ru>
2021-01-03 16:53:14 +0300

    Аутентификация, передача имени сотрудника и кнопка выхода

--- DB/tools.py
+++ DB/tools.py
@@ -1,25 +1,32 @@
 import sqlite3
 
 
-def connect(path='DB/myDB.db'):
-    try:
-        conn = sqlite3.connect(path)
-        print('Connected')
-        return conn
-    except sqlite3.Error as error:
-        print("Error while working with SQLite", error)
+class db_worker:
+    def __init__(self, path='DB/myDB.db'):
+        super().__init__()
+        self.conn = None
+        self.cur = None
+        self.connect(path)
 
+    def __del__(self):
+        self.cur.close()
+        self.conn.close()
 
-def check_login(conn, login, password):
-    try:
-        cur = conn.cursor()
-        cur.execute("SELECT fname, lname FROM employees WHERE login=? AND password=?", (login, password))
-        conn.commit()
-        ans = cur.fetchone()
-        print('Data received')
-        print(ans)
-        return ans
-    except sqlite3.Error as error:
-        print("Error while working with SQLite in <check_login>", error)
-    finally:
-        cur.close()
+    def connect(self, path='DB/myDB.db'):
+        try:
+            self.conn = sqlite3.connect(path)
+            self.cur = self.conn.cursor()
+            print('Connected')
+        except sqlite3.Error as error:
+            print("Error while working with SQLite", error)
+
+    def check_login(self, login, password):
+        try:
+            self.cur.execute("SELECT fname, lname FROM employees WHERE login=? AND password=?", (login, password))
+            self.conn.commit()
+            ans = self.cur.fetchone()
+            print('Data received')
+            print(ans)
+            return ans
+        except sqlite3.Error as error:
+            print("Error while working with SQLite in <check_login>", error)
--- myMain.py
+++ myMain.py
@@ -4,7 +4,7 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout, QMessageBox,
                              QMainWindow)
 
-import DB.tools as DB
+from DB.tools import db_worker
 
 
 class LoginForm(QWidget):
@@ -15,17 +15,35 @@ class LoginForm(QWidget):
         self.login_btn.clicked.connect(self.check_password)
 
     def check_password(self):
-        conn = DB.connect()
-        ans = DB.check_login(conn, self.login_text.text(), self.password_text.text())
+        DB = db_worker()
+        ans = DB.check_login(self.login_text.text(), self.password_text.text())
 
         msg = QMessageBox()
 
         if ans is not None:
-            msg.setText('Добро пожаловать,  '+ans[0]+'!')
-            msg.exec_()
+            self.form = MainWindow(ans)
+            self.form.show()
+            self.close()
+            # msg.setText('Добро пожаловать,  '+ans[0]+'!')
+            # msg.exec_()
+
         else:
             msg.setText('Данные введены не верно')
             msg.exec_()
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, employee):
+        super(MainWindow, self).__init__()
+        uic.loadUi('forms/MainWindow.ui', self)
+        self.employee_name_label.setText(f'{employee[0]}\n{employee[1]}')
+        self.exit_btn.clicked.connect(self.ext)
+
+    def ext(self):
+        self.form = LoginForm()
+        self.form.show()
+        self.close()
+
 
 
 if __name__ == '__main__':
